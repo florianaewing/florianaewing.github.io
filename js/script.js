916cac510c0b7c045586e5521a1fd868e7acf23a
florianaewing <florianaewing@gmail.com>
2026-09-14 17:50:24 -0700

    Revert "Add Stripe checkout, restructure artwork data, fix responsiveness"
    This reverts commit 1b928d0d5a1230971e6d7adcc2ac9e293f6f64bb.

--- js/script.js
+++ js/script.js
@@ -25,7 +25,5 @@ function showSlides(n) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
   slides[slideIndex-1].style.display = "block";
-  if (dots[slideIndex-1]) {
-    dots[slideIndex-1].className += " active";
-  }
+  dots[slideIndex-1].className += " active";
 }
